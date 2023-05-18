7776b740524b4bc07009aaaa4d5ae1a6d9e62eee
唯人 <yuito_it@outlook.jp>
2023-05-18 20:53:09 +0900

    Githubのwebhookのテストもかねてpush

--- Assets/Scripts/Game_main.cs
+++ Assets/Scripts/Game_main.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UE = UnityEngine;
 using UnityEngine;
 using System;
+using System.Diagnostics;
+using System.Threading;
 
 public class Game_main : MonoBehaviour
 {
@@ -9,22 +12,91 @@ public class Game_main : MonoBehaviour
     List<GameObject> BlackStone = new List<GameObject>();
     char[] posX = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' };
 
+    List<string> procInputStr = new List<string>();
 
     [SerializeField]
     GameObject whitePrefab;
     [SerializeField]
     GameObject blackPrefab;
 
+    SettingData settingData;
+
     // Start is called before the first frame update
     void Start()
     {
         GenStone();
+
+        //TODO:getsettingメソッド作る。
+        settingData = new SettingData();
+        settingData.komi = 5;
+        settingData.Black = true;
+        settingData.GTP[1].url = "C:\\Users\\sigum\\Documents\\ray-windows\\ray64.exe";
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    /// <summary>
+    /// GTPの通信管理コルーチン
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator GTP_Process_Manager()
+    {
+        UE.Debug.Log("Start:GTP_Ptocess_Manager");
+        ProcessStartInfo startInfo = new ProcessStartInfo()
+        {
+            FileName = settingData.GTP[1].url,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardError = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true
+        };
+
+        //意地でもusing使わないつもりの奴ww
+        Process process = new Process();
+        CancellationTokenSource cancellationToken = new CancellationTokenSource();
+        process.EnableRaisingEvents = true;
+
+
+        //アウトプット受け取るイベントハンドラー
+        process.OutputDataReceived += (sender, ev) =>
+        {
+            string output = ev.Data;
+            UE.Debug.Log($"Process:output={output}");
+            outputProcess(output);
+        };
+
+        //process終了した時の奴(そんなことないやろうけど)
+        process.Exited += (sender, ev) =>
+            {
+                UE.Debug.Log($"Process:exited");
+                cancellationToken.Cancel();
+            };
+        try
+        {
+            //TODO:ここに、クリック時の処理記述。
+            while (true)
+            {
+                //TODO:Listにwriteしたstringいれとく。
+            }
+        }
+        catch
+        {
+
+        }
+
+        finally
+        {
+            process?.Dispose();
+            cancellationToken?.Dispose();
+        }
+        yield return new WaitUntil(() => Input.GetMouseButtonUp(0));
+
+        yield return "test";
     }
 
     /// <summary>
@@ -87,7 +159,7 @@ public class Game_main : MonoBehaviour
         GameObject res = new GameObject();
         int posx;
         posx = Array.IndexOf(posX, Raw[0]);
-        Raw = Raw.Remove(0, 0);
+        Raw.Remove(0, 0);
         int posy;
         posy = int.Parse(Raw);
         if (Black)
@@ -111,4 +183,52 @@ public class Game_main : MonoBehaviour
     {
         return (int)x + 19 * (int)y;
     }
+
+    /// <summary>
+    /// アウトプットされたGTPのスクリプトを処理する。
+    /// </summary>
+    /// <param name="output">アウトプットされたデータを渡す。</param>
+    async void outputProcess(string output)
+    {
+        if (output.Length == 0 || output[0] == ' ') return;
+        if (output[0] == '?') UE.Debug.LogError("Error");
+        if (output[0] == '=')
+        {
+            output.Remove(0, 1);
+            if (Array.Exists<char>(posX, item => item == output[0]))
+            {
+                string param;
+                if (output[2] == ' ')
+                {
+                    param = output.Substring(0, 1);
+                }
+                else
+                {
+                    param = output.Substring(0, 2);
+                }
+                StringToObj(param, true);
+            }
+        }
+    }
+
+}
+
+
+
+/// <summary>
+/// setting.json読み込むときのクラス
+/// </summary>
+public class SettingData
+{
+    public GTP_Path_set[] GTP = new GTP_Path_set[1];
+    public int komi;
+    public bool Black;
+}
+
+/// <summary>
+/// GTPのパスのやつ
+/// </summary>
+public class GTP_Path_set
+{
+    public string url;
 }
